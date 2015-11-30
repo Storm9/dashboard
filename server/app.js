11fa16cf16233beba55a7eea98bd6a453cea0b76
Karthik <kkotha3@gmail.com>
2015-11-29 23:15:06 -0600

    https

--- server/app.js
+++ server/app.js
@@ -9,9 +9,21 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('express');
 var config = require('./config/environment');
+var fs = require('fs');
+
 // Setup server
 var app = express();
 var server = require('http').createServer(app);
+
+// This line is from the Node.js HTTPS documentation.
+var options = {
+  key: fs.readFileSync('./server/certs/key.pem'),
+  cert: fs.readFileSync('./server/certs/cert.pem')
+};
+
+// Create an HTTPS service identical to the HTTP service.
+var secureServer = require('https').createServer(options, app);
+
 require('./config/express')(app);
 require('./routes')(app);
 
@@ -19,6 +31,10 @@ require('./routes')(app);
 server.listen(config.port, config.ip, function () {
   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
+secureServer.listen('8000', config.ip, function () {
+  console.log('Secure Express server listening on %d, in %s mode', '8000', app.get('env'));
+});
+
 
 // Expose app
 exports = module.exports = app;
